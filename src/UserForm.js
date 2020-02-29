da8a3f7ec289cc2aaccb495d8da1f33ddbcc10a7
ericpkatz <ericpkatz@gmail.com>
2020-02-29 15:52:59 -0500

    commit

---
+++ src/UserForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const UserForm = ({ createUser })=> {
+  const [name, setName] = useState('');
+  const onSubmit = (ev)=> {
+    ev.preventDefault();
+    createUser({ name });
+    setName('');
+  };
+  return (
+    <section>
+      <form onSubmit={ onSubmit }>
+        <h2>Create User</h2>
+        <input value={ name } onChange={ ev => setName(ev.target.value)} />
+        <button>Create</button>
+      </form>
+    </section>
+  );
+};
+
+export default UserForm;
